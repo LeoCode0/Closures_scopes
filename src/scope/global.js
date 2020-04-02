1ececf802a5cec193399ac539194bd15012fe3aa
Leonardo Espinoza <leoespinozidk@gmail.com>
2020-04-01 22:53:07 -0600

    Global Scope

---
+++ src/scope/global.js
@@ -0,0 +1,13 @@
+var hello = 'Hello';
+var hello = 'Hello + world';
+let world = 'Hello world'
+const helloWorld = 'Hello World!'
+console.log(hello);
+
+const anotherFunction = () => {
+    console.log(hello);
+    console.log(world);
+    console.log(helloWorld);
+};
+
+anotherFunction();
